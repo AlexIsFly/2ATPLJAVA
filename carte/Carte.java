2883b8c525740a720d0368f9c183b35bd109fc29
AlexIsFly <pubox.ag@gmail.com>
2016-10-26 16:49:06 +0200

    Added fillCase + printMap methods These methods are to fill the map with correct position and type

--- carte/Carte.java
+++ carte/Carte.java
@@ -1,8 +1,33 @@
 package carte;
 
+import enumdata.NatureTerrain;
+
 /**
  * Created by alexisgacel on 21/10/2016.
  */
 public class Carte {
-    
+    private Case[][] map;
+    private int tailleCases;
+    private int nbLignes;
+    private int nbColonnes;
+
+    public Carte(int nbLignes, int nbColonnes, int tailleCases) {
+        this.tailleCases = tailleCases;
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.map = new Case[nbLignes][nbColonnes];
+    }
+
+    public void fillCase(int ligne, int colonne, NatureTerrain terrain) {
+        this.map[ligne][colonne] = new Case(ligne, colonne, terrain);
+    }
+
+    public void printMap() {
+        for (int lig = 0; lig < this.nbLignes; lig++) {
+            for (int col = 0; col < this.nbColonnes; col++) {
+                System.out.print(map[lig][col].getTerrain() + "  ");
+            }
+            System.out.println(lig);
+        }
+    }
 }
--- carte/Case.java
+++ carte/Case.java
@@ -1,43 +1,30 @@
 package carte;
 
+import enumdata.NatureTerrain;
+
 /**
  * Created by alexisgacel on 23/10/2016.
  */
-enum NatureTerrain {
-    FORET,
-    EAU,
-    ROCHE,
-    TERRAIN_LIBRE,
-    HABITAT;
-}
 
 public class Case {
     private int ligne;
     private int colonne;
     private NatureTerrain terrain;
 
-    public int getLigne() {
-        return ligne;
-    }
-
-    public void setLigne(int ligne) {
+    public Case(int ligne, int colonne, NatureTerrain terrain) {
         this.ligne = ligne;
-    }
-
-    public int getColonne() {
-        return colonne;
-    }
-
-    public void setColonne(int colonne) {
         this.colonne = colonne;
+        this.terrain = terrain;
+    }
+
+    public Case() {
+        this.ligne = 0;
+        this.colonne = 0;
+        this.terrain = NatureTerrain.EAU;
     }
 
     public NatureTerrain getTerrain() {
         return terrain;
-    }
-
-    public void setTerrain(NatureTerrain terrain) {
-        this.terrain = terrain;
     }
 }
 
