c9889275f2f8e8ac9fc420c5368963e94e44ff06
AlexIsFly <pubox.ag@gmail.com>
2016-10-26 16:34:15 +0200

    Put enum Types in a separate package

---
+++ enumdata/Direction.java
@@ -0,0 +1,11 @@
+package enumdata;
+
+/**
+ * Created by alexisgacel on 26/10/2016.
+ */
+public enum Direction {
+    NORD,
+    SUD,
+    EST,
+    OUEST,
+}
---
+++ enumdata/NatureTerrain.java
@@ -0,0 +1,12 @@
+package enumdata;
+
+/**
+ * Created by alexisgacel on 26/10/2016.
+ */
+public enum NatureTerrain {
+    FORET,
+    EAU,
+    ROCHE,
+    TERRAIN_LIBRE,
+    HABITAT,
+}
