36db1144a83bd2fdf303fc34893083e10d7b8cf9
AlexIsFly <pubox.ag@gmail.com>
2016-10-26 17:22:27 +0200

    Added Robot + Incendie classes

---
+++ incendie/Incendie.java
@@ -0,0 +1,7 @@
+package incendie;
+
+/**
+ * Created by alexisgacel on 26/10/2016.
+ */
+public class Incendie {
+}
---
+++ robot/Robots.java
@@ -0,0 +1,7 @@
+package robot;
+
+/**
+ * Created by alexisgacel on 26/10/2016.
+ */
+public class Robots {
+}
