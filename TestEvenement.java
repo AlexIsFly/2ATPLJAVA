5546006c0ff61a890e7d6771d070b27ab4f3d01d
AlexIsFly <xo2mmoE7iAYhfv>
2016-10-30 16:59:06 +0100

    Test classes for Terrain + Evenement

---
+++ TestEvenement.java
@@ -0,0 +1,17 @@
+import enumdata.Direction;
+import evenement.Evenement;
+import evenement.EvenementMoveDir;
+
+/**
+ * Created by alexisgacel on 30/10/2016.
+ * For Project Java ISSC - IMAG 2016
+ */
+public class TestEvenement {
+    public static void main(String[] args) {
+        Evenement event = new Evenement(1);
+        event.execute();
+
+        Evenement event2 = new EvenementMoveDir(1, Direction.EST);
+        event2.execute();
+    }
+}
