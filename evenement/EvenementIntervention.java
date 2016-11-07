3e899a24ac9ba4e9c491db754bb16ecf3b956246
LucasDorr <lucas-dorr@outlook.fr>
2016-11-07 14:44:26 +0100

    Update EvenementIntervention.java

--- evenement/EvenementIntervention.java
+++ evenement/EvenementIntervention.java
@@ -5,20 +5,20 @@ import enumdata.Direction;
 
 public class EvenementIntervention extends Evenement {
 	private Robots r ;
-	private Case pos ;
 
-	public EvenementIntervention(int date, Robots rob, Case c) {
+	public EvenementIntervention(int date, Robots rob) {
 		super(date);
 		this.r = rob;
-		this.pos = c;
 	}
 	
 	public void execute() {
 		int Qte;
+		Case pos;
+		pos=this.r.getPosition();
 		super.execute();
-		if ( this.pos.incendie == true ) {
-			Qte = min(this.r.getReservoir(),this.pos.getQteEau());  // la quantité que l'on va verser est le min entre ce que l'on peut verser et ce que l'on va verser
-			this.r.deverserEau(this.pos.getQteEau());
+		if ( pos.isIncendie()) {   // en réalité il faudrait vérifier si une des cases voisine est en feu 
+			Qte = min(this.r.getReservoir(),this.pos.getQteEau());  // la quantité que l'on va verser est le min entre ce que l'on peut verser et ce que l'on doit verser
+			this.r.deverserEau(Qte);
 			this.pos.setQteEau(this.pos.getQteEau()-Qte);
 			System.out.println(" Le robot a déversé "+ Qte +" volume d'eau ");
 		}
