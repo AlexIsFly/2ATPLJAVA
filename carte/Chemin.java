6c0fe6a465d0f85f832a9a45755c000a2d1b262e
AlexIsFly <pubox.ag@gmail.com>
2016-11-08 11:25:02 +0100

    Chemin new file

--- carte/Chemin.java
+++ carte/Chemin.java
@@ -1,38 +1,42 @@
+/*
+package carte;
 
-import enumdata.Directory;
+import carte.Case;
 import enumdata.NatureTerrain;
+import robot.Robots;
 
-public class chemin {
-	private Robots r;
-	private int[][] tab_poids;
+public class Chemin {
+    private Robots r;
+    private int[][] tab_poids;
 
 
-	public chemin(Robots rob) {
-		int i,j;
-		this.r=rob;
-	/*      i=rob.getGraphe().getLigne();
-		j=rob.getGraphe().getColonne(); */
-		this.tab_poids = new int[i][j];
-	}
+    public Chemin(Robots rob) {
+        int i,j;
+        this.r=rob;
+	      i=rob.getGraphe().getLigne();
+		j=rob.getGraphe().getColonne();
+        this.tab_poids = new int[i][j];
+    }
 
-	public int[][] getTab() {
-		return this.tab_poids;
-	}
-		
-	public Robots getRobots() {
-		return this.r;
-	}
+    public int[][] getTab() {
+        return this.tab_poids;
+    }
 
-	public void init_djikstra() {
-		Case pos;
-		int i,j;
-		pos=this.getRobots().getPosition()
-		i=this.getTab().length;
-		j=this.getTab()[0].length;
-		for( int k=0; k<i; k++) {
-			for( int l=0; l<j; l++){
-				this.getTab()[k][l]=10000; // on fixe tout les poids des sommets à l'infini
-			}
-		}
-		
-}
+    public Robots getRobots() {
+        return this.r;
+    }
+
+    public void init_djikstra() {
+        Case pos;
+        int i,j;
+        pos=this.getRobots().getPosition()
+        i=this.getTab().length;
+        j=this.getTab()[0].length;
+        for( int k=0; k<i; k++) {
+            for( int l=0; l<j; l++){
+                this.getTab()[k][l]=10000; // on fixe tout les poids des sommets à l'infini
+            }
+        }
+
+    }
+ */
